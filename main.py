7e58fbaf4aad707898bb5420eecc5a4c9f5dfd60
yuyu0127 <saiyuki01271210@gmail.com>
2019-07-16 16:06:32 +0900

    スコア表示・勝利

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ _intervalX	= 24
 _counterY	= 25
 _intervalY	= 26
 
+_memAddr	= 27
+_memBase	= 28
+
 # 初期化部分
 sys_init = [
 
@@ -48,19 +51,74 @@ Inst.ADDI(_counterX, _num0, 0),				# counterX = 0
 Inst.ADDI(_counterY, _num0, 0),				# counterY = 0
 Inst.ADDI(_intervalX, _num0, 3),			# interval = 1
 Inst.ADDI(_intervalY, _num0, 3),			# interval = 1
+
+# Store 7seg led patterns
+Inst.LUI(_memBase, 0x10001000),     # memory = 0x10001000
+Inst.ADDI(_tmp, 0, 0b11111100), 	# 7seg: 0
+Inst.SB(_memBase, _tmp, 0x0),
+Inst.ADDI(_tmp, 0, 0b01100000), 	# 1
+Inst.SB(_memBase, _tmp, 0x1),
+Inst.ADDI(_tmp, 0, 0b11011010), 	# 2
+Inst.SB(_memBase, _tmp, 0x2),
+Inst.ADDI(_tmp, 0, 0b11110010), 	# 3
+Inst.SB(_memBase, _tmp, 0x3),
+Inst.ADDI(_tmp, 0, 0b01100110), 	# 4
+Inst.SB(_memBase, _tmp, 0x4),
+Inst.ADDI(_tmp, 0, 0b10110110), 	# 5
+Inst.SB(_memBase, _tmp, 0x5),
+Inst.ADDI(_tmp, 0, 0b10111110), 	# 6
+Inst.SB(_memBase, _tmp, 0x6),
+Inst.ADDI(_tmp, 0, 0b11100000), 	# 7
+Inst.SB(_memBase, _tmp, 0x7),
+Inst.ADDI(_tmp, 0, 0b11111110), 	# 8
+Inst.SB(_memBase, _tmp, 0x8),
+Inst.ADDI(_tmp, 0, 0b11110110), 	# 9
+Inst.SB(_memBase, _tmp, 0x9),
+
+# 1P
+Inst.ADDI(_tmp, 0, 0x60), 
+Inst.SB(_addrBase, _tmp, 0x01), 
+Inst.ADDI(_tmp, 0, 0xCE), 
+Inst.SB(_addrBase, _tmp, 0x02), 
+# 2P
+Inst.ADDI(_tmp, 0, 0xDA), 
+Inst.SB(_addrBase, _tmp, 0x0D), 
+Inst.ADDI(_tmp, 0, 0xCE), 
+Inst.SB(_addrBase, _tmp, 0x0E), 
+# Point
+Inst.ADDI(_tmp, 0, 0xCE), 
+Inst.SB(_addrBase, _tmp, 0x10), 
+Inst.SB(_addrBase, _tmp, 0x1B), 
+Inst.ADDI(_tmp, 0, 0x3A), 
+Inst.SB(_addrBase, _tmp, 0x11), 
+Inst.SB(_addrBase, _tmp, 0x1C), 
+Inst.ADDI(_tmp, 0, 0x20), 
+Inst.SB(_addrBase, _tmp, 0x12), 
+Inst.SB(_addrBase, _tmp, 0x1D), 
+Inst.ADDI(_tmp, 0, 0x2A), 
+Inst.SB(_addrBase, _tmp, 0x13), 
+Inst.SB(_addrBase, _tmp, 0x1E), 
+Inst.ADDI(_tmp, 0, 0x1E), 
+Inst.SB(_addrBase, _tmp, 0x14), 
+Inst.SB(_addrBase, _tmp, 0x1F)
 ]
 
 
 game_init = [
-
 Inst.ADDI(_posX, _num0, 7),					# posX = 7
 Inst.ADDI(_posY, _num0, 1),					# posY = 1
-
 ]
 
 
 # ループ部分
 game_loop = [
+# スコア表示
+Inst.ADD(_memAddr, _score1, _memBase),
+Inst.LB(_tmp, _memAddr, 0),
+Inst.SB(_addrBase, _tmp, 0x21),
+Inst.ADD(_memAddr, _score2, _memBase),
+Inst.LB(_tmp, _memAddr, 0),
+Inst.SB(_addrBase, _tmp, 0x2E),
 
 # パターン初期化
 Inst.ADDI(_pattern0, _num0, 0),				# pattern0 = 0
@@ -88,56 +146,56 @@ Inst.SLL(_pattern3, _pattern3, _posX),		# 	pattern3 <<= posX
 
 # 左側のバーの表示
 Inst.ADDI(_tmp, _num0, 0),
-Inst.BNE(_barY1u, _tmp, 4),				# if barY1 == 0
+Inst.BNE(_barY1u, _tmp, 4),					# if barY1 == 0
 Inst.ORI(_pattern0, _pattern0, 2),			# 	pattern0 += 2
 Inst.ADDI(_tmp, _num0, 1),
-Inst.BNE(_barY1u, _tmp, 4),				# if barY1 == 1
+Inst.BNE(_barY1u, _tmp, 4),					# if barY1 == 1
 Inst.ORI(_pattern1, _pattern1, 2),			# 	pattern1 += 2
 Inst.ADDI(_tmp, _num0, 2),
-Inst.BNE(_barY1u, _tmp, 4),				# if barY1 == 2
+Inst.BNE(_barY1u, _tmp, 4),					# if barY1 == 2
 Inst.ORI(_pattern2, _pattern2, 2),			# 	pattern2 += 2
 Inst.ADDI(_tmp, _num0, 3),
-Inst.BNE(_barY1u, _tmp, 4),				# if barY1 == 3
+Inst.BNE(_barY1u, _tmp, 4),					# if barY1 == 3
 Inst.ORI(_pattern3, _pattern3, 2),			# 	pattern3 += 2
 
 Inst.ADDI(_tmp, _num0, 0),
-Inst.BNE(_barY1d, _tmp, 4),				# if barY1 == 0
+Inst.BNE(_barY1d, _tmp, 4),					# if barY1 == 0
 Inst.ORI(_pattern0, _pattern0, 2),			# 	pattern0 += 2
 Inst.ADDI(_tmp, _num0, 1),
-Inst.BNE(_barY1d, _tmp, 4),				# if barY1 == 1
+Inst.BNE(_barY1d, _tmp, 4),					# if barY1 == 1
 Inst.ORI(_pattern1, _pattern1, 2),			# 	pattern1 += 2
 Inst.ADDI(_tmp, _num0, 2),
-Inst.BNE(_barY1d, _tmp, 4),				# if barY1 == 2
+Inst.BNE(_barY1d, _tmp, 4),					# if barY1 == 2
 Inst.ORI(_pattern2, _pattern2, 2),			# 	pattern2 += 2
 Inst.ADDI(_tmp, _num0, 3),
-Inst.BNE(_barY1d, _tmp, 4),				# if barY1 == 3
+Inst.BNE(_barY1d, _tmp, 4),					# if barY1 == 3
 Inst.ORI(_pattern3, _pattern3, 2),			# 	pattern3 += 2
 
 # 右側のバーの表示
 Inst.ADDI(_tmp, _num0, 0),
-Inst.BNE(_barY2u, _tmp, 4),				# if barY2 == 0
+Inst.BNE(_barY2u, _tmp, 4),					# if barY2 == 0
 Inst.OR(_pattern0, _pattern0, _wr),			# 	pattern0 += wr
 Inst.ADDI(_tmp, _num0, 1),
-Inst.BNE(_barY2u, _tmp, 4),				# if barY2 == 1
+Inst.BNE(_barY2u, _tmp, 4),					# if barY2 == 1
 Inst.OR(_pattern1, _pattern1, _wr),			# 	pattern1 += wr
 Inst.ADDI(_tmp, _num0, 2),
-Inst.BNE(_barY2u, _tmp, 4),				# if barY2 == 2
+Inst.BNE(_barY2u, _tmp, 4),					# if barY2 == 2
 Inst.OR(_pattern2, _pattern2, _wr),			# 	pattern2 += wr
 Inst.ADDI(_tmp, _num0, 3),
-Inst.BNE(_barY2u, _tmp, 4),				# if barY2 == 3
+Inst.BNE(_barY2u, _tmp, 4),					# if barY2 == 3
 Inst.OR(_pattern3, _pattern3, _wr),			# 	pattern3 += wr
 
 Inst.ADDI(_tmp, _num0, 0),
-Inst.BNE(_barY2d, _tmp, 4),				# if barY2 == 0
+Inst.BNE(_barY2d, _tmp, 4),					# if barY2 == 0
 Inst.OR(_pattern0, _pattern0, _wr),			# 	pattern0 += wr
 Inst.ADDI(_tmp, _num0, 1),
-Inst.BNE(_barY2d, _tmp, 4),				# if barY2 == 1
+Inst.BNE(_barY2d, _tmp, 4),					# if barY2 == 1
 Inst.OR(_pattern1, _pattern1, _wr),			# 	pattern1 += wr
 Inst.ADDI(_tmp, _num0, 2),
-Inst.BNE(_barY2d, _tmp, 4),				# if barY2 == 2
+Inst.BNE(_barY2d, _tmp, 4),					# if barY2 == 2
 Inst.OR(_pattern2, _pattern2, _wr),			# 	pattern2 += wr
 Inst.ADDI(_tmp, _num0, 3),
-Inst.BNE(_barY2d, _tmp, 4),				# if barY2 == 3
+Inst.BNE(_barY2d, _tmp, 4),					# if barY2 == 3
 Inst.OR(_pattern3, _pattern3, _wr),			# 	pattern3 += wr
 
 # ドットマトリックスLEDの発光
@@ -151,12 +209,12 @@ Inst.SH(_addrBase, _pattern3, 0x46),		# [addrBase + 0x46] = pattern3
 Inst.ADDI(_tmp, _num0, 0),
 Inst.BNE(_posX, _tmp, 4 * 2),				# if posX == 0
 Inst.ADDI(_score2, _score2, 1),				# 	score2 += 1
-Inst.JAL(0, -4 * 79),										
+Inst.JAL(0, -4 * 85),										
 
 Inst.ADDI(_tmp, _num0, 15),
 Inst.BNE(_posX, _tmp, 4 * 2),				# if posX == 15
 Inst.ADDI(_score1, _score1, 1),				# 	score1 += 1
-Inst.JAL(0, -4 * 83),
+Inst.JAL(0, -4 * 88),
 
 Inst.ADDI(_counterX, _counterX, 1),
 Inst.BNE(_counterX, _intervalX, 4 * 30),
@@ -259,14 +317,34 @@ Inst.ADD(_barY2d, _barY2d, _tmp),			# 	barY2d += num1
 
 # 最後
 finalize = [
-
+Inst.ADDI(_tmp, _num0, 3),
+Inst.BEQ(_score1, _tmp, 4*2), 
+Inst.BEQ(_score2, _tmp, 4*1), 
 # ループの先頭に戻る
-Inst.JAL(0, -4 * ( len(game_loop) + 1 ) )
-
+Inst.JAL(0, -4 * ( len(game_loop) + 4 ) )
 ]
 
 
-program = sys_init + game_init + game_loop + finalize
+perform = [
+Inst.SB(_addrBase, 0, 0x01),
+Inst.SB(_addrBase, 0, 0x02),
+Inst.SB(_addrBase, 0, 0x0D),
+Inst.SB(_addrBase, 0, 0x0E),
+Inst.SB(_addrBase, 0, 0x10),
+Inst.SB(_addrBase, 0, 0x1B),
+Inst.SB(_addrBase, 0, 0x11),
+Inst.SB(_addrBase, 0, 0x1C),
+Inst.SB(_addrBase, 0, 0x12),
+Inst.SB(_addrBase, 0, 0x1D),
+Inst.SB(_addrBase, 0, 0x13),
+Inst.SB(_addrBase, 0, 0x1E),
+Inst.SB(_addrBase, 0, 0x14),
+Inst.SB(_addrBase, 0, 0x1F),
+Inst.SB(_addrBase, 0, 0x21),
+Inst.SB(_addrBase, 0, 0x2E),
+]
+
+program = sys_init + game_init + game_loop + finalize + perform
 
 # generate assembly
 #print('')
